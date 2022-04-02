42364cf3d5f9d75e349aa0f26794c694cf00ac76
DuarteCerdeira <duarte.cerdeira@tecnico.ulisboa.pt>
2022-04-02 17:00:59 +0100

    Aquire data

--- DAQ_Parte1.m
+++ DAQ_Parte1.m
@@ -2,3 +2,15 @@
 % 96195 - Duarte Cerdeira
 % Outros caralhos
 
+d = daq("ni");
+
+model_id = "";
+channel = 0;
+
+fa = 1000; % frequência de amostragem
+m = 0; % num de amostras
+
+addinput(d, model_id, channel, "Voltage");
+
+data = read(d, m, "OutputFormat", "Matrix");
+
