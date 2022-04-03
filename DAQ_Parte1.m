c402446cf7b44f9649952f207e4cf1334e5c320a
Andreoctavio <andre.octavio@gmail.com>
2022-04-03 18:16:23 +0100

    Valor medio e espetro

--- DAQ_Parte1.m
+++ DAQ_Parte1.m
@@ -24,34 +24,6 @@ addinput(d, model_id, channel, "Voltage");
 
 data = read(d, m, "OutputFormat", "Matrix");
 
-% =========== THD =========== %
-
-[f_fund_ef, f_fund] = max(tf_uni ./ sqrt(2)); % frequência fundamental e respetivo valor eficaz
-
-harm_ef = zeros(1, (fa / 2) / (f_fund - 1) - 1);
-
-for i = 2:(fa / 2) / (f_fund - 1) % número máximo de harmónicas possível de amostrar
-    harm_ef(i-1) = tf_uni(1 + (f_fund - 1) * i) / sqrt(2); % valor eficaz das harmónicas
-end
-
-sum_harm_amp= sum(harm_ef); % soma dos valores eficazes das harmónicas
-
-thd_dB = 10 * log10(sum_harm_amp / f_fund_amp); % THD em dB
-
-% =========== Diferença de fase =========== %
-
-u1 = cos(2*pi*50*t); % sinal 1
-u2 = cos(2*pi*50*t + pi/6); % sinal 2
-
-U1 = fft(u1); % transformada de fourier de sinal 1
-U2 = fft(u2); % transformada de fourier de sinal 2
-
-[~, index_1] = max(abs(U1)); % indíce da frequência fundamental do sinal 1
-[~, index_2] = max(abs(U2)); % indíce da frequência fundamental do sinal 2
-
-delta_phi = angle(U1(index_1)) - angle(U2(index_2)); % cálculo da diferença de fases
-
-
 % =========== Valor Eficaz =========== %
 
 media = mean((data.^2))
@@ -81,3 +53,41 @@ end
 
 plot(f, tf_uni);
 xlim([0 10])
+
+% =========== Valor médio =========== %
+
+valor_medio = mean (data);
+
+% =========== Espectro =========== %
+
+signal_espetro = tf_uni;
+signal_espetro_P = signal_espetro^2
+
+% =========== THD =========== %
+
+[f_fund_ef, f_fund] = max(tf_uni ./ sqrt(2)); % frequência fundamental e respetivo valor eficaz
+
+harm_ef = zeros(1, (fa / 2) / (f_fund - 1) - 1);
+
+for i = 2:(fa / 2) / (f_fund - 1) % número máximo de harmónicas possível de amostrar
+    harm_ef(i-1) = tf_uni(1 + (f_fund - 1) * i) / sqrt(2); % valor eficaz das harmónicas
+end
+
+sum_harm_amp= sum(harm_ef); % soma dos valores eficazes das harmónicas
+
+thd_dB = 10 * log10(sum_harm_amp / f_fund_amp); % THD em dB
+
+% =========== Diferença de fase =========== %
+
+u1 = cos(2*pi*50*t); % sinal 1
+u2 = cos(2*pi*50*t + pi/6); % sinal 2
+
+U1 = fft(u1); % transformada de fourier de sinal 1
+U2 = fft(u2); % transformada de fourier de sinal 2
+
+[~, index_1] = max(abs(U1)); % indíce da frequência fundamental do sinal 1
+[~, index_2] = max(abs(U2)); % indíce da frequência fundamental do sinal 2
+
+delta_phi = angle(U1(index_1)) - angle(U2(index_2)); % cálculo da diferença de fases
+
+
