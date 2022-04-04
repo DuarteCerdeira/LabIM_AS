fba72bc4de927c34849d8c6b3f3b8d2a397dcec3
DuarteCerdeira <duarte.cerdeira@tecnico.ulisboa.pt>
2022-04-04 21:03:44 +0100

    Noise power spectrum calculated

--- DAQ_Parte2.m
+++ DAQ_Parte2.m
@@ -4,14 +4,13 @@
 
 clear
 
-% sd = daq("ni");
-
 model_id = ""; % id do modelo
 channel = 0; % canal/canais a ler
 range = 10; % range do modelo
 
 fa = 10000; % frequência de amostragem
-n_samples = 10000; % num de amostras
+n_samples = 100000; % num de amostras
+n_measures = 50;
 
 res_espet = fa / n_samples; % resolução espetral
 res_temp = 1 / fa; % resolução temporal
@@ -19,6 +18,8 @@ res_temp = 1 / fa; % resolução temporal
 t = res_temp * (0:n_samples);
 
 % =========== Aquisição de sinais =========== %
+
+% sd = daq("ni");
 
 % addinput(d, model_id, channel, "Voltage");
 
@@ -28,82 +29,58 @@ t = res_temp * (0:n_samples);
 
 data = zeros(10, n_samples + 1);
 tf = zeros(10, n_samples + 1);
-tf_uni = zeros(10, n_samples / 2 + 1);
 
-for i = 1:10
-    data(i,:) = 0.8 * cos(2*pi*1000*t) + 0.010*randn(size(t));
-
+for i = 1:n_measures
+    data(i,:) = 0.8 * cos(2*pi*100*t) + 0.1*randn(size(t));    
     tf(i,:) = fft(data(i,:));
-    tf(i,:) = abs(tf(i,:)) / n_samples; % módulo da transformada de fourier
-    tf_uni(i,:) = tf(i,1:n_samples / 2 + 1); 
-    tf_uni(i,2:end-1) = 2*tf_uni(i,2:end-1); % transformada de fourier unilateral
 end
 
-tf_uni_av = mean(tf_uni);
-
 data_av = mean(data);
+
+tf = abs(tf) / n_samples; % módulo da transformada de fourier
+tf_av = mean(tf);
+tf_uni = tf_av(1:n_samples / 2 + 1); 
+tf_uni(2:end-1) = 2*tf_uni(2:end-1); % transformada de fourier unilateral
+
+% =========== Frequência =========== %
+
+[f_fund_ef, index] = max(tf_uni / sqrt(2)); % frequência fundamental e respetivo valor eficaz
+
+f_est = (sum(tf_uni(index-3:index+3) .* ((index-1) - 3:(index-1) + 3) .* res_espet)) / ...
+    sum(tf_uni(index-3:index+3)); % estimativa da frequência em caso de espalhamento espetral
+
+f_fund = (index - 1) * res_espet;
+
+% =========== Espectro de potência =========== %
+
+power_spect = 20 * log10(tf_uni); % espetro de potência unilateral
+power_spect_den = 10 * log10((tf_uni.^2) ./ res_espet); % densidade espetral de potência
+
+power_spect_noise = zeros(1,n_samples/2);
+
+for i = 1:n_samples / 2
+    if i == index
+        continue
+    else
+        power_spect_noise(i) = (tf_uni(i) ./ sqrt(2)) .^2; % espetro de potência do ruído
+    end
+end
+
+power_spect_noise_den = power_spect_noise / res_espet; % densidade espetral de potência do ruído
 
 % =========== Valor Eficaz =========== %
 
 valef = sqrt(mean(data_av.^2));
 
-noise_ef = mean(sqrt(mean((data(1:10, :) - data_av).^2)));
+noise_rms = sqrt((fa / 2) * mean(power_spect_noise_den));
 
-% =========== Frequência =========== %
+noise_floor = 10 * log10( (noise_rms^2) / (n_samples / 2) );
 
-% z(1,:) = cos(2*pi*2*t);
-% z(2,:) = cos(2*pi*3*t);
-% z(3,:) = cos(2*pi*5*t);
-
-% plot(t,z)
-
-[f_fund_ef, f_fund] = max(mean(tf_uni) / sqrt(2)); % frequência fundamental e respetivo valor eficaz
-
-f_est = (sum(tf_uni_av(f_fund-3:f_fund+3) .* (f_fund-3 - 1:f_fund+3 - 1) .* res_espet)) / ...
-    sum(tf_uni_av(f_fund-3:f_fund+3)); 
-
-%f_fund = (f_fund - 1) * res_espet;
-
-% =========== Valor médio =========== %
-
-% valor_medio = mean (data_av)
-
-% =========== Espectro de potência =========== %
-
-power_spect(1:10,:) = 20 * log10(tf_uni(1:10,:));
-
-power_spect_den(1:10,:) = 10 * log10((tf_uni(1:10,:).^2) ./ res_espet);
-
-% =========== THD =========== %
-
-% harm_ef = zeros(1, (fa / 2) / (f_fund - 1) - 1);
-
-% for i = 2:(fa / 2) / (f_fund - 1) % número máximo de harmónicas possível de amostrar
-%     harm_ef(i-1) = tf_uni(1 + (f_fund - 1) * i) / sqrt(2); % valor eficaz das harmónicas
-% end
-
-% sum_harm_amp= sum(harm_ef); % soma dos valores eficazes das harmónicas
-
-% thd_dB = 10 * log10(sum_harm_amp / f_fund_amp); % THD em dB
-
-% =========== Diferença de fase =========== %
-
-% u1 = cos(2*pi*50*t); % sinal 1
-% u2 = cos(2*pi*50*t + pi/6); % sinal 2
-
-% U1 = fft(u1); % transformada de fourier de sinal 1
-% U2 = fft(u2); % transformada de fourier de sinal 2
-
-% [~, index_1] = max(abs(U1)); % indíce da frequência fundamental do sinal 1
-% [~, index_2] = max(abs(U2)); % indíce da frequência fundamental do sinal 2
-
-% delta_phi = angle(U1(index_1)) - angle(U2(index_2)); % cálculo da diferença de fases
+% =========== PLOT =========== %
 
 f = res_espet * (0:n_samples/2);
 
-noise_rms = sqrt((fa / 2) * 10 .^ (mean(mean(power_spect_den)) ./ 10));
-
-plot(f, mean(power_spect));
+plot(f, power_spect);
 
 %figure(1);
 
