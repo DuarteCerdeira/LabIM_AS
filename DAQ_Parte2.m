ab770127da33a2a18f3e7c68d2f6fe36d990c699
DuarteCerdeira <duarte.cerdeira@tecnico.ulisboa.pt>
2022-04-04 23:35:33 +0100

    DAQ_Parte2 finished

--- DAQ_Parte2.m
+++ DAQ_Parte2.m
@@ -8,9 +8,9 @@ model_id = ""; % id do modelo
 channel = 0; % canal/canais a ler
 range = 10; % range do modelo
 
-fa = 10000; % frequência de amostragem
-n_samples = 100000; % num de amostras
-n_measures = 50;
+fa = 500000; % frequência de amostragem
+n_samples = 500000; % num de amostras
+n_measures = 10;
 
 res_espet = fa / n_samples; % resolução espetral
 res_temp = 1 / fa; % resolução temporal
@@ -31,7 +31,7 @@ data = zeros(10, n_samples + 1);
 tf = zeros(10, n_samples + 1);
 
 for i = 1:n_measures
-    data(i,:) = 0.8 * cos(2*pi*100*t) + 0.1*randn(size(t));    
+    data(i,:) = 10 * cos(2*pi*500*t) + 0.01*randn(size(t));    
     tf(i,:) = fft(data(i,:));
 end
 
@@ -44,7 +44,9 @@ tf_uni(2:end-1) = 2*tf_uni(2:end-1); % transformada de fourier unilateral
 
 % =========== Frequência =========== %
 
-[f_fund_ef, index] = max(tf_uni / sqrt(2)); % frequência fundamental e respetivo valor eficaz
+[f_fund_amp, index] = max(tf_uni / sqrt(2)); % frequência fundamental e respetiva amplitude
+
+f_fund_power = 20 * log10(f_fund_amp);
 
 f_est = (sum(tf_uni(index-3:index+3) .* ((index-1) - 3:(index-1) + 3) .* res_espet)) / ...
     sum(tf_uni(index-3:index+3)); % estimativa da frequência em caso de espalhamento espetral
@@ -53,8 +55,8 @@ f_fund = (index - 1) * res_espet;
 
 % =========== Espectro de potência =========== %
 
-power_spect = 20 * log10(tf_uni); % espetro de potência unilateral
-power_spect_den = 10 * log10((tf_uni.^2) ./ res_espet); % densidade espetral de potência
+power_spect = (tf_uni / sqrt(2)).^2; % espetro de potência unilateral
+power_spect_den = 10 * log10(((tf_uni / sqrt(2)).^2) ./ res_espet); % densidade espetral de potência
 
 power_spect_noise = zeros(1,n_samples/2);
 
@@ -62,7 +64,7 @@ for i = 1:n_samples / 2
     if i == index
         continue
     else
-        power_spect_noise(i) = (tf_uni(i) ./ sqrt(2)) .^2; % espetro de potência do ruído
+        power_spect_noise(i) = (tf_uni(i) / sqrt(2)).^2; % espetro de potência do ruído
     end
 end
 
@@ -78,18 +80,19 @@ noise_floor = 10 * log10( (noise_rms^2) / (n_samples / 2) );
 
 % =========== PLOT =========== %
 
+SINAD = f_fund_power - 10*log10(sum(power_spect_noise_den.*res_espet));
+ENOB = ceil((SINAD - 1.76) / 6.02);
+
 f = res_espet * (0:n_samples/2);
 
-plot(f, power_spect);
+figure(1);
 
-%figure(1);
+subplot(2, 1, 1);
+plot(t(1:1000), data(1, 1:1000));
+title("Dados adquiridos", "Valor eficaz: " + valef + " | Valor eficaz do ruído: " + noise_rms +...
+    " | SINAD: " + SINAD + " | ENOB: " + ENOB + ...
+    " | Número de amostras: " + n_samples + " | Frequência de amostragem: " + fa + " | Alcance: " + range);
 
-%subplot(2, 1, 1);
-%plot(t(1:100), data(1, 1:100));
-%title("Dados adquiridos", "Frequência: " + f_fund + " | Valor eficaz do ruído: " + noise_ef +...
-%    " | Valor eficaz: " + valef + " | Número de amostras: " + n_samples +  ...
-%    " | Frequência de amostragem: " + fa + " | Alcance: " + range);
-
-%subplot(2, 1, 2);
-%plot(f, power_spect);
+subplot(2, 1, 2);
+plot(f, 10*log10(power_spect));
 
